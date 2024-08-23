058c7cd0619c7e7a08ae6296f8caa4c7cfdab557
Nikita <makofthesky@gmail.com>
2024-08-23 04:29:31 +0300

    accounting was started

---
+++ src/main/java/telran/java53/accounting/dto/UserRegisterDto.java
@@ -0,0 +1,13 @@
+package telran.java53.accounting.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserRegisterDto {
+	String login;
+	
+	String firstName;
+	String lastName;
+	String password;
+	
+}
